1bae227e98417b0f91521b36e1dd4d1509f2aa76
Jorge Trad <jetradl@gmail.com>
2021-02-26 12:18:57 -0600

    Form to create users and validate them is now available

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,8 +19,36 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
-                    
-                    <table class="table">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach ($errors->all() as $error)
+                                    - {{ $error }} <br>
+                                    @endforeach
+                                </div>
+                            @endif
+                            <form action="{{ route('users.store') }}" method="POST">
+                                <div class="row g-3 align-items-center">
+                                    <div class="col-sm-3">
+                                        <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name') }}">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Password">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <table class="table table-striped table-hover">
                         <thead>
                             <tr>
                                 <th>ID</th>
